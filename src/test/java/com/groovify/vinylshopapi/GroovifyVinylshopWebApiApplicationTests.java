b6faaa14779c9db39e3fd1046cfe4873fcf51a6a
AnneKluytmans <annekluytmans1@gmail.com>
2025-01-22 12:09:47 +0100

    Project initialize

---
+++ src/test/java/com/groovify/vinylshopapi/GroovifyVinylshopWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groovify.vinylshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroovifyVinylshopWebApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
