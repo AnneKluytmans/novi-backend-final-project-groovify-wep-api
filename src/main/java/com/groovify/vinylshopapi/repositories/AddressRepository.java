507be3a2f39cb2b6d7d80d8e0959d7364f5dc51d
AnneKluytmans <annekluytmans1@gmail.com>
2025-03-05 11:08:48 +0100

    Implemented GET requests to get all addresses and customer shipping addresses with filter and sort options

--- src/main/java/com/groovify/vinylshopapi/repositories/AddressRepository.java
+++ src/main/java/com/groovify/vinylshopapi/repositories/AddressRepository.java
@@ -2,13 +2,14 @@ package com.groovify.vinylshopapi.repositories;
 
 import com.groovify.vinylshopapi.models.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface AddressRepository extends JpaRepository<Address, Long> {
+public interface AddressRepository extends JpaRepository<Address, Long>, JpaSpecificationExecutor<Address> {
     List<Address> findAllByCustomerId(Long customerId);
     Optional<Address> findByEmployeeId(Long employeeId);
 
